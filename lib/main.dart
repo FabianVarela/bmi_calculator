f4b79329b5093b1fb3d562e46df06302c044b16f
FabianVarela <jfabian.303@gmail.com>
2020-04-10 00:28:08 -0500

    Modify main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:bmi_calculator/ui/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      title: "BMI",
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI',
       home: HomeBMI(),
-    ));
+    );
+  }
+}
