cc4b30ac192bec200d6c8bb99e7b96c6bf7ca390
FabianVarela <jfabian.303@gmail.com>
2020-04-12 13:54:26 -0500

    Modify custom card

--- lib/ui/common/custom_card.dart
+++ lib/ui/common/custom_card.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  CustomCard({@required this.title, @required this.child});
+  CustomCard({
+    @required this.title,
+    @required this.child,
+    this.padding = const EdgeInsets.all(10),
+  });
 
   final String title;
   final Widget child;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +20,7 @@ class CustomCard extends StatelessWidget {
       child: Container(
         height: double.infinity,
         width: double.infinity,
-        padding: EdgeInsets.all(10),
+        padding: padding,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
@@ -29,9 +34,11 @@ class CustomCard extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 12),
-              child: child,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(top: 12),
+                child: child,
+              ),
             ),
           ],
         ),
