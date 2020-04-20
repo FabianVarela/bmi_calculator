8a5fe9904d25669fc7abc691f059a752ccbf7e25
FabianVarela <jfabian.303@gmail.com>
2020-04-19 19:44:58 -0500

    Modify result UI screen

--- lib/ui/result.ui.dart
+++ lib/ui/result.ui.dart
@@ -24,7 +24,7 @@ class _ResultUIState extends State<ResultUI> {
           Container(height: MediaQuery.of(context).size.height),
           Positioned.fill(
             child: Container(
-              padding: EdgeInsets.fromLTRB(24, 50, 24, 10),
+              padding: EdgeInsets.fromLTRB(24, 50, 24, 120),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               child: IntervalColumnBottomAnimation(
@@ -33,81 +33,108 @@ class _ResultUIState extends State<ResultUI> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 30),
-                    child: Text(
-                      'BMI final result',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    elevation: 20,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 30),
-                      child: Text(
-                        '22.4',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 100,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Overweight',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'BMI = 22.93 kg/m2',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          'BMI weight range for the height:',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        Text(
-                          '128.9 lbs - 174.2 lbs',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      _setIconButton(Icons.delete, () {}),
-                      _setIconButton(Icons.refresh, () {}, isCircle: true),
-                      _setIconButton(Icons.share, () {}),
-                    ],
-                  ),
+                  _setTitle(),
+                  _setCardResult(),
+                  _setTextResult(),
+                  _setMessage(),
+                  _setOptionIcons(),
                 ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: _setArcButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _setTitle() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20, bottom: 30),
+      child: Text(
+        'BMI final result',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w700,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget _setCardResult() {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      elevation: 20,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        child: Text(
+          '22.4',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 100,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _setTextResult() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Text(
+        'Overweight',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 35,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+  }
+
+  Widget _setMessage() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 5),
+            child: Text(
+              'BMI = 22.93 kg/m2',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 5),
+            child: Text(
+              'BMI weight range for the height:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 5),
+            child: Text(
+              '128.9 lbs - 174.2 lbs',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w200,
               ),
             ),
           ),
@@ -116,20 +143,70 @@ class _ResultUIState extends State<ResultUI> {
     );
   }
 
+  Widget _setOptionIcons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        _setIconButton(Icons.save, () => print('Save data')),
+        _setIconButton(
+          Icons.refresh,
+          () => Navigator.pop(context),
+          isCircle: true,
+        ),
+        _setIconButton(Icons.share, () => print('Share data')),
+      ],
+    );
+  }
+
   Widget _setIconButton(IconData icon, Function function,
       {bool isCircle = false}) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10),
+      padding: EdgeInsets.all(5),
       decoration: isCircle
           ? BoxDecoration(
               color: Colors.blueAccent,
-              shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(50),
             )
           : null,
       child: IconButton(
-        icon: Icon(icon),
         onPressed: function,
-        color: isCircle ? Colors.white : Colors.grey,
+        padding: EdgeInsets.zero,
+        icon: Icon(
+          icon,
+          size: 30,
+          color: isCircle ? Colors.white : Colors.blueGrey,
+        ),
+      ),
+    );
+  }
+
+  Widget _setArcButton() {
+    return Align(
+      alignment: Alignment.center,
+      child: FractionalTranslation(
+        translation: Offset(0.0, 0.6),
+        child: GestureDetector(
+          onTap: () => print('My profile'),
+          child: Container(
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              border: Border.all(color: Colors.white, width: 2),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Icon(
+                Icons.account_circle,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
