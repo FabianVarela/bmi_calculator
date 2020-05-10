58dba9e0b3ccf0a8d60e9be8ed4af6eb7745afe7
FabianVarela <jfabian.303@gmail.com>
2020-05-10 12:51:30 -0500

    Add ensure initialized method

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await SystemChrome.setPreferredOrientations(
       <DeviceOrientation>[DeviceOrientation.portraitUp]);
 
