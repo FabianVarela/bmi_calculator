0ad40643d8960884d79776269be57cf955f7c99a
fabianvarela <jfabian.303@gmail.com>
2020-05-24 16:14:24 -0500

    Change font family by text theme adding google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bmi_calculator/ui/home.ui.dart';
 import 'package:bmi_calculator/ui/utils/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +23,9 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BMI',
-      theme: ThemeData(fontFamily: 'FiraSans'),
+      theme: ThemeData(
+        textTheme: GoogleFonts.firaSansTextTheme(Theme.of(context).textTheme),
+      ),
       home: Builder(builder: (BuildContext context) {
         final double width = MediaQuery.of(context).size.width;
         final double height = MediaQuery.of(context).size.height;
